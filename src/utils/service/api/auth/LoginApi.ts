229da62c2e9bd4ca13f3d26b1af66eaccdc9dd28
Oveiss <oveiss.ershadian@gmail.com>
2025-10-07 14:45:40 +0330

    add console

--- src/utils/service/api/auth/LoginApi.ts
+++ src/utils/service/api/auth/LoginApi.ts
@@ -1,12 +1,16 @@
 import http from "../../interceptor/axiosClient";
 import { ILoginRequest, ILoginResponse } from "../../../../types/LoginTypes";
 
-const LoginApi = async (data: ILoginRequest): Promise<ILoginResponse> => {
+const LoginApi = async (data: ILoginRequest): Promise<ILoginResponse | null> => {
   try {
     const res = await http.post<ILoginResponse>("/auth/login", data);
+    console.log("Response data:", res.data.accessToken);
+
     return res.data;
+    
   } catch (error) {
-    return { success: false, message: "خطا در لاگین" };
+     console.error("LoginApi Error:", error);
+  return null;
   }
 };
 
