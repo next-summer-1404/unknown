9accb793ed60c51586cd9bc0e10b81a89061aac6
rezvan <rezvane@gmail.com>
2025-10-07 22:24:55 +0330

    6 day

---
+++ src/app/(public)/houseReserve/page.tsx
@@ -0,0 +1,12 @@
+import BaseReserve from '@/components/houseReserve/BaseReserve'
+import React from 'react'
+
+const HouseReservePage = () => {
+  return (
+    <div>
+      <BaseReserve/>
+    </div>
+  )
+}
+
+export default HouseReservePage
