2cfa358fea308fb53e85cb07dec3b825e93a22d6
Oveiss <oveiss.ershadian@gmail.com>
2025-10-07 18:31:34 +0330

    .

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import DiscountSection from "@/components/HomeContainer/DiscountSection/DiscountSection";
+import HeroSection from "@/components/HomeContainer/HeroSection/heroSection";
+import PropertyCategories from "@/components/HomeContainer/PropertyCategories/PropertyCategories";
+import Rental from "@/components/HomeContainer/Rental/Rental";
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="">
+      <HeroSection />
+      <PropertyCategories />
+      <DiscountSection />
+      <Rental />
+    </div>
+  );
+};
+
+export default Landing;
--- src/components/auth/login/LoginPage.tsx
+++ src/components/auth/login/LoginPage.tsx
@@ -31,11 +31,10 @@ const LoginPage = () => {
       Cookies.set("accessToken", result.accessToken, { expires: 1 });
       Cookies.set("refreshToken", result.refreshToken, { expires: 1 });
       // console.log( "token", result.accessToken);
-     
 
-      router.push("/about");
+      router.push("/dashboard");
     } else {
-      alert( "ورود ناموفق بود");
+      alert("ورود ناموفق بود");
     }
 
     setLoading(false);
