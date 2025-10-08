fe85adeea4fc4ec0fce567f6a4f989e0d446e5aa
Oveiss <oveiss.ershadian@gmail.com>
2025-10-08 20:56:15 +0330

    .

--- src/components/HomeContainer/AboutUs/AboutUs.tsx
+++ src/components/HomeContainer/AboutUs/AboutUs.tsx
@@ -26,29 +26,29 @@ const AboutUs = () => {
         </h3>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-        <div className="flex flex-col gap-5 items-center justify-center px-6  ">
+      <div className="grid grid-cols-1  md:grid-cols-3 gap-4 p-4">
+        <div className="flex flex-col gap-5 items-center justify-center px-4  ">
           <div className="relative bg-[#2D2D2D] rounded-3xl h-1/2 min-h-36 p-4 w-full">
-          <div
-            className={`${styles.cardShape} w-9/12 h-7 absolute top-0 left-0 z-50 bg-[#232323] rounded-[0_0_40px_0]`}
-          ></div>
-          <div className="relative rounded-3xl  bg-[#444444] h-full overflow-hidden pt-7">
             <div
-              className={`${styles.insideCardShape} w-[79%] h-7 absolute top-0 left-0 z-0 bg-[#2D2D2D]  rounded-[0_0_40px_0]`}
+              className={`${styles.cardShape} w-9/12 h-7 absolute top-0 left-0 z-50 bg-[#232323] rounded-[0_0_40px_0]`}
             ></div>
-            <div className="h-full bg-amber-50"></div>
-          </div>
+            <div className="relative rounded-3xl  bg-[#444444] h-full overflow-hidden pt-7">
+              <div
+                className={`${styles.insideCardShape} w-[79%] h-7 absolute top-0 left-0 z-0 bg-[#2D2D2D]  rounded-[0_0_40px_0]`}
+              ></div>
+              <div className="h-full bg-amber-50"></div>
+            </div>
           </div>
           <div className="relative bg-[#2D2D2D] rounded-3xl h-1/2 min-h-36 p-4 w-full">
-          <div
-            className={`${styles.cardShape} w-9/12 h-7 absolute top-0 left-0 z-50 bg-[#232323] rounded-[0_0_40px_0]`}
-          ></div>
-          <div className="relative rounded-3xl  bg-[#444444] h-full overflow-hidden pt-7">
             <div
-              className={`${styles.insideCardShape} w-[79%] h-7 absolute top-0 left-0 z-0 bg-[#2D2D2D]  rounded-[0_0_40px_0]`}
+              className={`${styles.cardShape} w-9/12 h-7 absolute top-0 left-0 z-50 bg-[#232323] rounded-[0_0_40px_0]`}
             ></div>
-            <div className="h-full bg-amber-50"></div>
-          </div>
+            <div className="relative rounded-3xl  bg-[#444444] h-full overflow-hidden pt-7">
+              <div
+                className={`${styles.insideCardShape} w-[79%] h-7 absolute top-0 left-0 z-0 bg-[#2D2D2D]  rounded-[0_0_40px_0]`}
+              ></div>
+              <div className="h-full bg-amber-50"></div>
+            </div>
           </div>
         </div>
         <div className="relative bg-[#2D2D2D] rounded-3xl h-full p-4 ">
@@ -70,28 +70,28 @@ const AboutUs = () => {
             </p>
           </div>
         </div>
-        <div className=" flex flex-col gap-5 items-center justify-center px-6 rounded-xl text-center sm:col-span-3 md:col-span-1">
+        <div className=" flex flex-col gap-5 items-center justify-center px-4  md:col-span-1">
           <div className="relative bg-[#2D2D2D] rounded-3xl h-1/2 min-h-36 p-4 w-full">
-          <div
-            className={`${styles.cardShape} w-9/12 h-7 absolute top-0 left-0 z-50 bg-[#232323] rounded-[0_0_40px_0]`}
-          ></div>
-          <div className="relative rounded-3xl  bg-[#444444] h-full overflow-hidden pt-7">
             <div
-              className={`${styles.insideCardShape} w-[79%] h-7 absolute top-0 left-0 z-0 bg-[#2D2D2D]  rounded-[0_0_40px_0]`}
+              className={`${styles.cardShape} w-9/12 h-7 absolute top-0 left-0 z-50 bg-[#232323] rounded-[0_0_40px_0]`}
             ></div>
-            <div className="h-full bg-amber-50"></div>
-          </div>
+            <div className="relative rounded-3xl  bg-[#444444] h-full overflow-hidden pt-7">
+              <div
+                className={`${styles.insideCardShape} w-[79%] h-7 absolute top-0 left-0 z-0 bg-[#2D2D2D]  rounded-[0_0_40px_0]`}
+              ></div>
+              <div className="h-full bg-amber-50"></div>
+            </div>
           </div>
           <div className="relative bg-[#2D2D2D] rounded-3xl h-1/2 min-h-36 p-4 w-full">
-          <div
-            className={`${styles.cardShape} w-9/12 h-7 absolute top-0 left-0 z-50 bg-[#232323] rounded-[0_0_40px_0]`}
-          ></div>
-          <div className="relative rounded-3xl  bg-[#444444] h-full overflow-hidden pt-7">
             <div
-              className={`${styles.insideCardShape} w-[79%] h-7 absolute top-0 left-0 z-0 bg-[#2D2D2D]  rounded-[0_0_40px_0]`}
+              className={`${styles.cardShape} w-9/12 h-7 absolute top-0 left-0 z-50 bg-[#232323] rounded-[0_0_40px_0]`}
             ></div>
-            <div className="h-full bg-amber-50"></div>
-          </div>
+            <div className="relative rounded-3xl  bg-[#444444] h-full overflow-hidden pt-7">
+              <div
+                className={`${styles.insideCardShape} w-[79%] h-7 absolute top-0 left-0 z-0 bg-[#2D2D2D]  rounded-[0_0_40px_0]`}
+              ></div>
+              <div className="h-full bg-amber-50"></div>
+            </div>
           </div>
         </div>
       </div>
