b95b609fae33dcb688e6914ae69a7ed5b75a750e
Oveiss <oveiss.ershadian@gmail.com>
2025-10-11 15:17:07 +0330

    footer done.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import styles from "./Footer.module.css";
-import { PhoneIcon } from "@heroicons/react/24/outline";
+import {
+  AtSymbolIcon,
+  MapPinIcon,
+  PhoneIcon,
+} from "@heroicons/react/24/outline";
 import Image from "next/image";
 import BlackArrow from "../../assets/images/blackArrow.png";
-import Person from "../../assets/images/personFooter.png"
-import { ChevronLeftIcon } from "@heroicons/react/24/solid";
+import Person from "../../assets/images/personFooter.png";
+import Logo1 from "../../assets/images/logo1.png";
+import Logo2 from "../../assets/images/logo2.png";
+import Logo3 from "../../assets/images/logo3.png";
+import Linked from "../../assets/images/linked.png";
+import Insta from "../../assets/images/insta.png";
+import Whats from "../../assets/images/whatsapp.png";
+import Telegram from "../../assets/images/telegram1.png";
+import { ChevronLeftIcon, PlayIcon } from "@heroicons/react/24/solid";
 const Footer = () => {
   return (
     <div className="bg-[#232323]  p-9">
@@ -97,10 +108,104 @@ const Footer = () => {
             </form>
           </div>
           <div className="w-full md:w-5/12 flex justify-around items-end ">
-          <Image src={Person} alt="" width={400} />
-          
+            <Image src={Person} alt="" width={400} />
           </div>
-          <div className="w-full bg-indigo-950 h-52">ddddd</div>
+          <div className="flex flex-col gap-4 w-full bg-[#232323] text-white rounded-4xl overflow-hidden p-7">
+            <h2 className="text-2xl">پلتفرم دلتا</h2>
+            <div className="flex flex-col md:flex-row gap-6">
+              <div className="w-[26%]">
+                <p className="">
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                  استفاده از طراحان گرافیک است.لورم ایپسوم متن ساختگی با تولید
+                  سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک
+                  است.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت ..
+                </p>
+              </div>
+              <div className="w-[17%] px-3.5 flex flex-col gap-3">
+                <h1 className="">نحوه رزرو اقامتگاه</h1>
+
+                <h3 className=" w-fit text-xs flex items-center gap-3 text-[#AAAAAA] hover:text-white cursor-pointer group">
+                  <PlayIcon className="w-4 h-4 rotate-180 hidden group-hover:inline-block " />
+                  راهنمای رزرو اقامتگاه
+                </h3>
+                <h3 className=" w-fit text-xs flex items-center gap-3 text-[#AAAAAA] hover:text-white cursor-pointer group">
+                  <PlayIcon className="w-4 h-4 rotate-180 hidden group-hover:inline-block " />
+                  شیوه پرداخت
+                </h3>
+                <h3 className=" w-fit text-xs flex items-center gap-3 text-[#AAAAAA] hover:text-white cursor-pointer group">
+                  <PlayIcon className="w-4 h-4 rotate-180 hidden group-hover:inline-block " />
+                  لغو رزرو اقامتگاه
+                </h3>
+              </div>
+              <div className="w-[17%] px-3.5 flex flex-col gap-3">
+                <h1 className="">خدمات مشتریان</h1>
+
+                <h3 className=" w-fit text-xs flex items-center gap-3 text-[#AAAAAA] hover:text-white cursor-pointer group">
+                  <PlayIcon className="w-4 h-4 rotate-180 hidden group-hover:inline-block " />
+                  پرسش های متداول مهمان
+                </h3>
+                <h3 className=" w-fit text-xs flex items-center gap-3 text-[#AAAAAA] hover:text-white cursor-pointer group">
+                  <PlayIcon className="w-4 h-4 rotate-180 hidden group-hover:inline-block " />
+                  پرسش های متداول میزبان
+                </h3>
+                <h3 className=" w-fit text-xs flex items-center gap-3 text-[#AAAAAA] hover:text-white cursor-pointer group">
+                  <PlayIcon className="w-4 h-4 rotate-180 hidden group-hover:inline-block " />
+                  چطور اقامتگاه ثبت کنم ؟
+                </h3>
+                <h3 className=" w-fit text-xs flex items-center gap-3 text-[#AAAAAA] hover:text-white cursor-pointer group">
+                  <PlayIcon className="w-4 h-4 rotate-180 hidden group-hover:inline-block " />
+                  حریم شخصی کاربران
+                </h3>
+              </div>
+              <div className="w-fit px-3.5 flex flex-col gap-3">
+                <h1 className="">راه ارتباطی دلتا</h1>
+
+                <h3 className=" w-fit text-xs flex items-center gap-2 text-[#AAAAAA] ">
+                  <PhoneIcon className="w-4 h-4" />
+                  09229167194 - 098541612310
+                </h3>
+                <h3 className=" w-fit text-xs flex items-center gap-2 text-[#AAAAAA]">
+                  <AtSymbolIcon className="w-4 h-4" />
+                  Delta@gmail.com
+                </h3>
+                <h3 className=" w-fit text-xs flex items-center gap-2 text-[#AAAAAA]">
+                  <MapPinIcon className="w-4 h-4" />
+                  گیلان ، رشت ، میدان آزادی ، جنب چهار راه عظیمی زاده
+                </h3>
+              </div>
+            </div>
+            <div className="flex items-center gap-5 px-2">
+              <div className="flex items-center justify-center bg-white rounded-xl w-13 h-13 overflow-hidden">
+                <Image src={Logo1} alt="" width={50} className="w-10 h-10" />
+              </div>
+              <div className="flex items-center justify-center border-white border-1 rounded-xl w-13 h-13 overflow-hidden">
+                <Image src={Logo2} alt="" width={50} className="w-10 h-10" />
+              </div>
+              <div className="flex items-center justify-center border-white border-1 rounded-xl w-13 h-13 overflow-hidden">
+                <Image src={Logo3} alt="" width={50} className="w-9 h-9" />
+              </div>
+            </div>
+            <div className="bg-[#AAAAAA] h-0.5 w-full"></div>
+            <div className="flex items-center justify-between">
+              <h4 className="">
+                تمام حقوق مادی و معنوی این اثر برای دلتا محفوظ است .
+              </h4>
+              <div className="flex items-center gap-3">
+                <div className="flex items-center justify-center bg-white rounded-xl w-10 h-10 overflow-hidden">
+                  <Image src={Linked} alt="" width={50} className="w-8 h-8" />
+                </div>
+                <div className="flex items-center justify-center border-white border-1 rounded-xl w-10 h-10 overflow-hidden">
+                  <Image src={Insta} alt="" width={18} className="w-5 h-5" />
+                </div>
+                <div className="flex items-center justify-center border-white border-1 rounded-xl w-10 h-10 overflow-hidden">
+                  <Image src={Telegram} alt="" width={18} className="w-5 h-5" />
+                </div>
+                <div className="flex items-center justify-center border-white border-1 rounded-xl w-10 h-10 overflow-hidden">
+                  <Image src={Whats} alt="" width={18} className="w-5 h-5" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
