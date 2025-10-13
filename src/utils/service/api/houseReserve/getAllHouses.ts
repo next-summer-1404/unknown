e4fa83c19a0b5bf6d471149465df8b9dc72d77e3
rezvan <rezvane@gmail.com>
2025-10-13 19:20:23 +0330

    10 day

--- src/utils/service/api/houseReserve/getAllHouses.ts
+++ src/utils/service/api/houseReserve/getAllHouses.ts
@@ -2,7 +2,6 @@ import { HouseReserveTypes } from "@/types/HouseReserveTypes";
 import http from "../../interceptor/axiosClient";
 
 export const getAllHouses = async (): Promise<HouseReserveTypes[]> => {
-  const res = await http.get("/api/houses");
-  // طبق Postman: houses داخل data هست
+  const res = await http.get("/houses");
   return res.data.houses;
 };
