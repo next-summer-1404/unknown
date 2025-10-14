83a057e8412b63914ce557cc72089db86a0b71d8
Oveiss <oveiss.ershadian@gmail.com>
2025-10-14 09:48:34 +0330

    flex-col

--- src/components/houseReserve/house/BaseReserve.tsx
+++ src/components/houseReserve/house/BaseReserve.tsx
@@ -33,7 +33,7 @@ const BaseReserve = () => {
   return (
     <div className="w-full h-auto border border-amber-300">
       <TopFilter />
-      <div className="w-11/12 m-auto h-[1080px] mt-5 bg-[#2A2A2A] p-4 rounded-xl  flex gap-5">
+      <div className="w-11/12 m-auto mt-5 bg-[#2A2A2A] p-4 rounded-xl  flex flex-col gap-5">
         <div className="w-3/5 h-auto">
           <BottomFilter />
           {houses.map((house) => (
