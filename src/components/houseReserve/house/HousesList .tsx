8e6b6ab9626bcdddd213de9117e10b3cbe233423
Oveiss <oveiss.ershadian@gmail.com>
2025-10-14 09:56:10 +0330

    correct

--- src/components/houseReserve/house/HousesList .tsx
+++ src/components/houseReserve/house/HousesList .tsx
@@ -1,49 +1,34 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { getAllHouses } from "../../../utils/service/api/houseReserve/getAllHouses";
 import CardReserve from "../../../components/houseReserve/house/CardReserve";
-import { HouseReserveTypes } from "../../../types/HouseReserveTypes";
 import toast from "react-hot-toast";
+import { IHouses } from "@/types/IHouses";
+import { getHouses } from "@/utils/service/api/getAllHouses";
 
 const HousesList = () => {
-  const [House, setHouse] = useState<HouseReserveTypes[]>([]);
-  const [totalCount, setTotalCount] = useState<number>(0);
+  const [houses, setHouses] = useState<IHouses[]>([]);
+  const [number, setNumber] = useState();
+  console.log(houses);
+  console.log(number);
 
-  //   useEffect(() => {
-  //     (async () => {
-  //       try {
-  //         const { houses, totalCount } = await getAllHouses();
-  //         setHouses(houses);
-  //         totalCount;
-  //         console.log(totalCount,'kkkkkkk')
-  //         setTotalCount(totalCount);
-  //       } catch (error) {
-  //         toast.error("خطا در بارگذاری لیست خانه‌ها");
-  //       }
-  //     })();
-  //   }, []);
-
-  const houseHandle = async () => {
+  const GetHouses = async () => {
     try {
-      const res = await getAllHouses();
-      setHouse(res.houses);
-    //   console.log(res.houses,'ffffff')
-    //   totalCount;
-      setTotalCount(res.totalCount);
-    //   console.log(res.totalCount,'ffffff')
-
+      const result = await getHouses({ page: 1, limit: 100 });
+      setHouses(result.houses);
+      setNumber(result.totalCount);
     } catch (error) {
+      console.error(error);
       toast.error("خطا در بارگذاری  ");
     }
   };
   useEffect(() => {
-    houseHandle();
+    GetHouses();
   }, []);
 
   return (
     <div className="w-full flex flex-col gap-10">
-      <p className="text-white text-lg">تعداد کل خانه‌ها: {totalCount}</p>
-      {House.map((house) => (
+      <p className="text-white text-lg">تعداد کل خانه‌ها: {number}</p>
+      {houses.map((house) => (
         <CardReserve key={house.id} house={house} />
       ))}
     </div>
