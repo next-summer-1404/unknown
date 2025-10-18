0da374dc8bdcb284fc588863f471fe3a7be7c731
Oveiss <oveiss.ershadian@gmail.com>
2025-10-18 22:02:35 +0330

    .

--- src/types/ICommonCardProps.ts
+++ src/types/ICommonCardProps.ts
@@ -1,5 +1,34 @@
 export interface ICommonCardProps {
-  id?: number;
+    id: string;
+  title: string;
+  address: string;
+  photos: string[];
+  rate: string;
+  discounted_price: string;
+  price: string;
+  tags: string[];
+  last_updated: string;
+  capacity: number;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  categories: {
+    name: string;
+  };
+  bathrooms: number;
+  parking: number;
+  rooms: number;
+  yard_type: string;
+  num_comments: number;
+  discount_id: string;
+  transaction_type: string;
+  sellerId: string;
+  sellerName: string;
+  caption: string;
+  bookings: number;
+  favoriteId: string | null;
+  isFavorite: boolean;
   // title: string;
   // discount?: string;
   // location?: string;
