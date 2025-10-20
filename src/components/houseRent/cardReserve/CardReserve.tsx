c2ec9f79099d76a1d6cc20f8508c06465d8801c5
rezvan <rezvane@gmail.com>
2025-10-20 17:02:27 +0330

    14 day

--- src/components/houseRent/cardReserve/CardReserve.tsx
+++ src/components/houseRent/cardReserve/CardReserve.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CardReserve = () => {
   return (
-    <div className='w-11/12 h-[1000px] border border-red-300 mx-auto flex'>
+    <div className='w-11/12 h-[1000px] border border-red-300 mx-auto flex'>{/*اینجا*/}
       <div className='w-1/2 h-60 border border-amber-400'></div>
       <div className='w-1/2 h-60 border border-blue-400'></div>
     </div>
