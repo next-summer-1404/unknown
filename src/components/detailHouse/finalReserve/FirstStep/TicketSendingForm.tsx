19c782e02e2dc62f0b147e2de50cbd76b633eb52
Oveiss <oveiss.ershadian@gmail.com>
2025-10-22 19:25:54 +0330

    ticket sending done

--- src/components/detailHouse/finalReserve/FirstStep/TicketSendingForm.tsx
+++ src/components/detailHouse/finalReserve/FirstStep/TicketSendingForm.tsx
@@ -1,57 +1,58 @@
-
 import { CheckCircleIcon, UsersIcon } from "@heroicons/react/24/outline";
 const TicketSendingForm = () => {
   return (
-     <div className=" h-auto bg-[#393939]  rounded-2xl ">
-        <div className="p-2 rounded-2xl bg-[#4D4D4D] flex gap-1 items-center">
-          <UsersIcon className="w-5 h-5 mr-2 text-[#FFFFFF]" />
-          <p className="text-[#FFFFFF]"> ارسال بلیط به دیگری</p>
-          <span className="text-sm text-[#8CFF45]">
-            (ارسال بلیط به ایمیل و شماره همراه دیگر)
-          </span>
-        </div>
-        <form className=" w-full h-auto flex ">
-          <div className=" w-1/2 flex gap-2 mt-5">
-            {/* شماره تلفن */}
-            <div className=" w-1/2 flex flex-col justify-center">
-              <label htmlFor="phone" className="mr-4 text-[#AAAAAA]">
-                شماره تلفن
-              </label>
-              <input
-                id="phone"
-                name="phone"
-                type="tel"
-                placeholder="09110000000"
-                className="border border-[#AAAAAA] text-[#AAAAAA] outline-none p-2 rounded-2xl bg-transparent mr-2"
-              />
-            </div>
-
-            {/* ایمیل */}
-            <div className=" w-1/2  flex flex-col justify-center">
-              <label htmlFor="email" className="mr-2 text-[#AAAAAA]">
-                ایمیل
-              </label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="example@email.com"
-                className="border border-[#AAAAAA] text-[#AAAAAA] outline-none p-2 rounded-2xl bg-transparent"
-              />
-            </div>
-          </div>
-          <div className="w-1/2 flex">
-            <div className="w-1/2 "></div>
-            <div className="w-1/2 ">
-              <button className="flex items-center border border-[#8CFF45] rounded-xl p-2 gap-1 mt-8 mr-50">
-                <CheckCircleIcon className="w-5 h-5 text-[#8CFF45]" />
-                <p className="text-[#8CFF45] text-sm">ثبت اطلاعات</p>
-              </button>
-            </div>
-          </div>
-        </form>
+    <div className="py-2 px-2 bg-[#393939]  rounded-2xl ">
+      <div className="p-2 rounded-2xl bg-[#4D4D4D] flex gap-1 items-center">
+        <UsersIcon className="w-5 h-5 mr-2 text-[#FFFFFF]" />
+        <p className="text-[#FFFFFF]"> ارسال بلیط به دیگری</p>
+        <span className="text-sm text-[#8CFF45]">
+          (ارسال بلیط به ایمیل و شماره همراه دیگر)
+        </span>
       </div>
-  )
-}
+      <form className=" w-full py-7 px-2 flex items-center justify-between">
+        <div className=" w-1/2 flex gap-2">
+          {/* شماره تلفن */}
+          <div className="w-1/2 relative">
+            <label
+              htmlFor="username"
+              className="absolute right-5 -top-2 bg-[#393939] px-1 text-[#aaaaaa] text-sm"
+            >
+              شماره تلفن:
+            </label>
+            <input
+              type="text"
+              id="phone"
+              name="phone"
+              placeholder="وارد کنید..."
+              className="w-full border border-[#aaaaaa] text-[#aaaaaa] rounded-3xl px-3 py-2 focus:border-blue-500 focus:outline-none"
+            />
+          </div>
 
-export default TicketSendingForm
+          {/* ایمیل */}
+          <div className="w-1/2 relative">
+            <label
+              htmlFor="email"
+              className="absolute right-5 -top-2 bg-[#393939] px-1 text-[#aaaaaa] text-sm"
+            >
+              ایمیل:
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="وارد کنید..."
+              className="w-full border border-[#aaaaaa] text-[#aaaaaa] rounded-3xl px-3 py-2 focus:border-blue-500 focus:outline-none"
+            />
+          </div>
+        </div>
+
+        <button className="flex items-center border border-[#8CFF45] rounded-2xl p-2 gap-2 cursor-pointer">
+          <CheckCircleIcon className="w-5 h-5 text-[#8CFF45]" />
+          <p className="text-[#8CFF45] text-sm">ثبت اطلاعات</p>
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default TicketSendingForm;
