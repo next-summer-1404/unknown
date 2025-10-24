a46583fda266c2d99e4c1173a756367b28c68ad2
Oveiss <oveiss.ershadian@gmail.com>
2025-10-24 13:26:44 +0330

    edit names

--- src/utils/service/api/getHouseDetail.ts
+++ src/utils/service/api/getHouseDetail.ts
@@ -2,6 +2,7 @@ import http from "../interceptor/axiosClient";
 import { IHouses } from "@/types/IHouses";
 
 export const getHouseDetail = async (id: string): Promise<IHouses> => {
+  console.log(id, "aaa")
   const res = await http.get<IHouses>(`/houses/${id}`);
   return res;
 };
