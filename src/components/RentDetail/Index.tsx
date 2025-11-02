386474389b4c43d81dce38b21cf862df7c9cb59c
Oveiss <oveiss.ershadian@gmail.com>
2025-11-02 12:41:28 +0330

    name Changed

--- src/components/RentDetail/Index.tsx
+++ src/components/RentDetail/Index.tsx
@@ -9,10 +9,10 @@ interface Props {
   house: IHouses;
 }
 
-const BaseDetailRent: React.FC<Props> = ({ house }) => {
+const Index: React.FC<Props> = ({ house }) => {
   return (
-    <div className="border border-[#232323] px-18 w-full">
-      <div className="w-full h-auto mt-30 flex gap-5 ">
+    <div className="px-18 pt-30 w-full">
+      <div className="w-full flex gap-5 ">
         <div className="w-5/12 h-96 border border-red-400 rounded-2xl"></div>
         <div className="w-4/12 h-96 flex flex-col gap-5">
           <div className="w-full h-1/2  border border-green-400 rounded-2xl"></div>
@@ -27,4 +27,4 @@ const BaseDetailRent: React.FC<Props> = ({ house }) => {
   );
 };
 
-export default BaseDetailRent;
+export default Index;
