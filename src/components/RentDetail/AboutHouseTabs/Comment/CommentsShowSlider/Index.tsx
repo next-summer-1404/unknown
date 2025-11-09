b9a2670fa20e2d1240c4647a66b9f0a87ff47daf
Oveiss <oveiss.ershadian@gmail.com>
2025-11-09 10:00:09 +0330

    .

--- src/components/RentDetail/AboutHouseTabs/Comment/CommentsShowSlider/Index.tsx
+++ src/components/RentDetail/AboutHouseTabs/Comment/CommentsShowSlider/Index.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import CommentSlider from './CommentSlider'
+import { getComments } from '@/utils/service/api/getComments';
+import { IComments } from '@/types/ICommentCardProps';
 
-const Index = () => {
+const Index =  () => {
+
+  // اینجا
+//   const comments =  getComments({
+//   page: 1,
+//   limit: 10,
+// });
+// const Comment: IComments[] = comments;
+
+console.log(Comment)
   return (
     <div className='pt-11 pb-3'>
       <CommentSlider />
